94087627f2edcdd4742bed6132940631a16a0f13
Daniel <danlikestech@gmail.com>
2020-04-15 17:21:48 -0500

    deleted HTML docs, added templates & installed handlebars (hbs)

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,24 +1,46 @@
 const path = require('path'),
-express = require('express')
+    express = require('express')
 
 app = express()
 
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
 
 
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Daniel Kamara'
+    })
+})
 
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Daniel Kamara'
+    })
+})
+
+
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        text: 'This is a message to see if things worked.'
+    })
+})
 
 
 app.get('/weather', (req, res) => {
     res.send({
         name: 'Texas',
         weather: 'cold'
-    },)
+    }, )
 })
 
 
