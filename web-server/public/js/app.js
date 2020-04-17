2bcddbb5a03ee872e97abad9b4d498f6c8dc0376
Daniel <danlikestech@gmail.com>
2020-04-16 22:34:05 -0500

    migrated the fetch call into the submit callback

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,13 +1,20 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const location = search.value
 
 
-
-fetch('http://localhost:3000/weather?address=!').then((res) => {
-res.json().then((data) => {
-if (data.err) {
-console.log(data.err)
-} else {
-console.log(data.location)
-console.log(data.forecast)
-}
-})
+    fetch('http://localhost:3000/weather?address=!').then((res) => {
+        res.json().then((data) => {
+            if (data.err) {
+                console.log(data.err)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
+    })
 })
