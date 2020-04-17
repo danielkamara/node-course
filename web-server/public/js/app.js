37ab2ba48889eb9b752d1e0f4861854e9f275ca1
Daniel <danlikestech@gmail.com>
2020-04-16 22:57:35 -0500

    created error message plus added feature where user can add address to search bar

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,19 +1,29 @@
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const msg1 = document.querySelector('#message-1')
+const msg2 = document.querySelector('#message-2')
+
+
+msg1.textContent = 'From JavaScript'
+
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
 
     const location = search.value
 
+        msg1.textContent = 'Loading...'
+        msg2.textContent = ''
+
+
 
     fetch('http://localhost:3000/weather?address=' + location).then((res) => {
         res.json().then((data) => {
             if (data.err) {
-                console.log(data.err)
+                msg1.textContent = data.err
             } else {
-                console.log(data.location)
-                console.log(data.forecast)
+                msg1.textContent = data.location
+                msg2.textContent = data.forecast
             }
         })
     })
