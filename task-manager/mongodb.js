a1f21efe68289cc7c7a68a88448cdb24375d2a79
Daniel <danlikestech@gmail.com>
2020-04-19 09:16:54 -0500

    started task-manager app

---
+++ task-manager/mongodb.js
@@ -0,0 +1,23 @@
+// CRUD Create Read Update Delete
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }, (err, client) => {
+if (err) {
+    return console.log('Unable to connect to database!')
+} 
+
+const db = client.db(databaseName)
+db.collection('users').insertOne({
+    name: 'Daniel',
+    age: 200
+})
+})
