827d18cb56c37d4b7eb6597ba5fb2de47ff1d2c0
Daniel <danlikestech@gmail.com>
2020-04-21 12:27:05 -0500

    set up REST api route

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose'),
-    validator = require('validator')
+const mongoose = require('mongoose')
+  
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
@@ -7,65 +7,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true,
     useUnifiedTopology: true
 })
-
-// Add a password field to User
-// 1. Setup the field as a required string
-// 2. Ensure the length is greater than 6
-// 3. Trim the password
-// 4. Ensure the password doesn't contain "password"
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain "password"')
-            }
-        }
-    }
-})
-
-// const me = new User({
-//     name: '     Daniel     ',
-//     email: ' MyEmail@kamaraSON.io    ',
-//     password: 'dsaasdasd123'
-// })
-
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((err) => {
-//     console.log('Error!', err)
-// })
-
 
 
 // 1. Define the model description and completed fields
@@ -80,15 +21,3 @@ const Task = mongoose.model('Task', {
         default: false
     }
 })
-
-// 2. Create a new instance of the model
-const task = new Task({
-    description: 'Eat dinner'
-})
-
-// 3. Save the model to the database
-task.save().then(() => {
-console.log(task)
-}).catch((err) => {
-console.log(err)
-}) 
---
+++ task-manager/src/models/user.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose'),
+    validator = require('validator')
+
+
+
+
+// Add a password field to User
+// 1. Setup the field as a required string
+// 2. Ensure the length is greater than 6
+// 3. Trim the password
+// 4. Ensure the password doesn't contain "password"
+
+
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
+    }
+})
+
+
+
+module.exports = User
