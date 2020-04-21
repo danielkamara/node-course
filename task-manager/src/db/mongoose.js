ea84a928fe7b869b3d7fd7a52d395d62daca4917
Daniel <danlikestech@gmail.com>
2020-04-20 20:14:19 -0500

    set up email validation using npm package validator

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose'),
+    validator = require('validator')
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
@@ -9,31 +10,51 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
-// const me = new User({
-//     name: 'Daniel',
-//     age: 36
-// })
+const me = new User({
+    name: '     Daniel     ',
+    email: ' MyEmail@kamaraSON.io    '
+})
 
 
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((err) => {
-//     console.log('Error!', err)
-// })
+me.save().then(() => {
+    console.log(me)
+}).catch((err) => {
+    console.log('Error!', err)
+})
 
 
 
 // 1. Define the model description and completed fields
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true
     },
     completed: {
         type: Boolean
@@ -41,14 +62,14 @@ const Task = mongoose.model('Task', {
 })
 
 // 2. Create a new instance of the model
-const task = new Task({
-    description: 'Learn the Mongoose library',
-    completed: false
-})
+// const task = new Task({
+//     description: 'Learn the Mongoose library',
+//     completed: false
+// })
 
 // 3. Save the model to the database
-task.save().then(() => {
-console.log(task)
-}).catch((err) => {
-console.log(err)
-}) 
+// task.save().then(() => {
+// console.log(task)
+// }).catch((err) => {
+// console.log(err)
+// }) 
