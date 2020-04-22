a253644217b1c1ead3d37f4010b928e51382555c
Daniel <danlikestech@gmail.com>
2020-04-22 10:24:53 -0500

    added promise-chaining.js to project

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -48,8 +48,6 @@ app.get('/users/:id', (req, res) => {
 
 
 
-
-
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
@@ -60,6 +58,30 @@ app.post('/tasks', (req, res) => {
     })
 })
 
+
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+
+})
 
 
 app.listen(port, () => {
