4e6f397ae87bc369ef6c0a97143edc3fc2537ed6
Daniel <danlikestech@gmail.com>
2020-04-23 09:09:58 -0500

    created route for user login

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -3,14 +3,6 @@ const mongoose = require('mongoose'),
     bcrypt = require('bcryptjs')
 
 
-
-
-// Add a password field to User
-// 1. Setup the field as a required string
-// 2. Ensure the length is greater than 6
-// 3. Trim the password
-// 4. Ensure the password doesn't contain "password"
-
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -19,6 +11,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -49,6 +42,25 @@ const userSchema = new mongoose.Schema({
         }
     }
 })
+
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({
+        email
+    })
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+    return user
+}
+
+//Hash the plain text password before saving
+
 
 userSchema.pre('save', async function (next) {
     const user = this
