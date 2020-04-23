f6df2367787b8bf6a5fd972e2e622b28a1f5ba72
Daniel <danlikestech@gmail.com>
2020-04-23 17:43:16 -0500

    setup middleware all request to database

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,9 +8,28 @@ app = express()
 port = process.env.PORT || 3000
 
 
+
+
+// app.use((req, res, next) => {
+//    if (req.method === 'GET') {
+//     res.send('GET request are disabled')
+//    }else {
+//        next()
+//    }
+// })
+
+app.use((req, res, next) => {
+    res.status(503).send('Site is under maintenance, Come back later!')
+})
+
+
+
+
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+
 
 
 app.listen(port, () => {
