c028721f088f4df1ddc1b6445517e6af826e911c
Daniel <danlikestech@gmail.com>
2020-04-28 22:06:32 -0500

    created a user/task relationship

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,11 +7,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-
-
-
-
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -22,4 +17,20 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+// const task = await Task.findById('5ea8e25bf6f3c305eb6eedd2')
+// await task.populate('owner').execPopulate()
+// console.log(task.owner)
+
+
+const user = await User.findById('5ea8e121f6f3c305eb6eedd0')
+await user.populate('task').execPopulate()
+console.log(user.task)
+}
+
+main()
 
